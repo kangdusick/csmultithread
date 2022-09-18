5ebdf0f12aec58863a29bafeb684a39c808275df
taejun <kdsgamestudio@gmail.com>
2022-09-18 13:52:37 +0900

    소켓 프로그래밍 입문 #2

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -1,19 +1,49 @@
-﻿namespace DummyClient
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DummyClient
 {
     class Program
     {
-        static void MainThread()
-        {
-            while (true)
-                Console.WriteLine("1111");
-        }
         static void Main()
         {
-            Thread t = new Thread(MainThread);
-            t.Start();
-            Console.WriteLine("2222");
-            t.Join();
-            Console.WriteLine("asdf");
+            //DNS (Domain name System)
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            //휴대폰 설정
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                //문지기한테 입장 문의
+                socket.Connect(endPoint);
+                Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
+
+                //보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
+                int sendBytes = socket.Send(sendBuff);
+
+                //받는다
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Server] {recvData}");
+
+                //나간다
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+           
         }
     }
 }
