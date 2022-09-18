717a02aafb8a8ebdd4709a011e0c3c1db7a6cfb7
taejun <kdsgamestudio@gmail.com>
2022-09-18 12:03:32 +0900

    소켓추가중

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,44 +1,23 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
 namespace ServerCore
 {
+   
     class Program
     {
-        static int x;
-        static int y;
-        static int r1;
-        static int r2;
-        static void Thread1()
+        static ThreadLocal<string> ThreadName = new();
+        static void WhoAmI()
         {
-            y = 1;
-            Thread.MemoryBarrier();
-            r1 = x;
-        }
-        static void Thread2()
-        {
-            x = 1;
-            Thread.MemoryBarrier();
-            r2 = y;
+
         }
         static void Main()
         {
-            int count = 0;
-            while (true)
-            {
-                count++;
-                x = y = r1 = r2 = 0;
-                var t1 = new Task(Thread1);
-                var t2 = new Task(Thread2);
-                t1.Start();
-                t2.Start();
-                Task.WaitAll(t1, t2);
-                if(r1==0&&r2==0)
-                {
-                    break;
-                }
-            }
-           
-            Console.WriteLine(count.ToString());
+            //DNS (Domain name System)
+            string host = Dns.GetHostName();
+            Socket listenSocket = new Socket();
         }
     }
 }
